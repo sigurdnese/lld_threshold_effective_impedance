e6d8e17cc4596fc159e0722a477df14c831bbbae
Sigurd Nese <sigurdnese@hotmail.com>
2021-08-12 17:17:21 +0200

    added funcs and params files

---
+++ effective_impedance_funcs.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mpmath
+from scipy.special import jv
+from effective_impedance_params import *
+
+def resonator_impedance(K,quality,resonance_freq):
+    shunt = ImZ_over_k*quality*resonance_freq/f_0
+    if resonance_freq != 0:
+        return shunt/(1+1j*(quality*((K*f_0)/(resonance_freq)-(resonance_freq)/(K*f_0))))
+    else:
+        # Handle apparent singularity
+        return np.zeros_like(K)
+
+# Constant inductive impedance
+def constant_inductive(K):
+    return 1j*K*ImZ_over_k
+
+def hg1f2(Mu,Y):
+    return float(mpmath.hyp1f2(0.5,2,Mu,-Y**2))
+
+def hg2f3(Mu,Y):
+    return float(mpmath.hyp2f3(0.5,0.5,1.5,2,Mu,-Y**2))
+
+def G_kk(Mu,Y,Phi_max):
+    return np.complex64(1j*((16*Mu*(Mu+1))/(np.pi*Phi_max**4)*(1-hg1f2(Mu,Y))))
+
+def make_G_diag(Mu,Y,Phi_max):
+    if Mu == 0.5:
+        print('mu=0.5, using Bessel functions')
+        G_diag = np.complex64(1j*((16*Mu*(Mu+1))/(np.pi*Phi_max**4)*(1-jv(1,2*Y)/Y)))
+    elif Mu == 2:
+        print('mu=2, using Bessel functions')
+        G_diag = np.complex64(1j*((16*Mu*(Mu+1))/(np.pi*Phi_max**4)*(0.5-(jv(0,Y))**2-(jv(1,Y))**2+jv(0,Y)*jv(1,Y)/Y)))
+    else:
+        G_diag = []
+        step = 0
+        total = len(Y)
+        for yy in Y:
+            step += 1
+            if step%1000 == 0:
+                print(f'{step} of {total}',end='\r')
+            G_diag.append(G_kk(Mu,yy,Phi_max))
+        print()
+        G_diag = np.array(G_diag)
+    return G_diag
+
+def get_effective_impedance(imag_Z_k,Mu,Phi_max,K,K_bool=np.array([True]),G_diag=[]):
+    Y = K*Phi_max/h
+    # G_kk is pure imaginary. i will cancel, so take imag to save memory
+    if len(G_diag)==0:
+        G_diag = np.imag(make_G_diag(Mu,Y,Phi_max))
+    # Sum over last axis, works for impedance grid and single list
+    numerator = np.sum(G_diag*imag_Z_k/K,axis=-1,where=K_bool)
+    if len(imag_Z_k.shape)>1:
+        G_diag_grid = np.array([[G_diag for i in range(imag_Z_k.shape[0])] for j in range(imag_Z_k.shape[1])])
+        denominator = np.sum(G_diag_grid,axis=-1,where=K_bool)
+    else:
+        denominator = np.sum(G_diag,axis=-1,where=K_bool)
+    return numerator/denominator
+def numerator_cumsum(imag_Z_k,Mu,Phi_max,K,G_diag=[]):
+    # Creates G_kk array, takes impedance model array
+    Y = K*Phi_max/h
+    if len(G_diag)==0:
+        G_diag = make_G_diag(Mu,Y,Phi_max)
+    return np.cumsum(G_diag*imag_Z_k/K)
+
+def chi(Mu, Y):
+    return Y*(1-hg2f3(Mu,Y))
+
+def get_xi_threshold(Mu,Y_max,Effective_Impedance,Phi_max):
+    # Equation (55) in paper
+    return (np.pi*Phi_max**5*ImZ_over_k)/(32*Mu*(Mu+1)*chi(Mu,Y_max)*Effective_Impedance)
+
---
+++ effective_impedance_params.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Parameters
+f_rf = 400.79e6 # Hz
+f_c = 1*f_rf # Cutoff frequency
+h = 35640 
+f_0 = f_rf/h
+mu = 2
+V_0 = 6e6 # V
+qV_0 = V_0*1 # eV
+phi_s0 = np.pi # Synchronous phase
+phi_max = 1.3
+gamma_tr = 55.76
+E_0 = 0.45e12 # ev
+
+# phi_max_array = np.linspace(0.8,2,20)
+
+# Broadband resonator impedance
+Q = 1
+f_r = 10*f_rf
+ImZ_over_k = 0.07 # Ohm
